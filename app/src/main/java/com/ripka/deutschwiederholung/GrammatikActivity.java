ce75742ed68885e363414dac181711ba1220f527
rsemenyshyn <r.semenyshyn.ua@gmail.com>
2016-12-28 18:44:20 +0100

    - some style updated - grammar section added (reading of pdf)

---
+++ app/src/main/java/com/ripka/deutschwiederholung/GrammatikActivity.java
@@ -0,0 +1,8 @@
+package com.ripka.deutschwiederholung;
+
+/**
+ * Created by ripka on 12/28/16.
+ */
+
+public class GrammatikActivity extends NavActivity {
+}
--- app/src/main/java/com/ripka/deutschwiederholung/NounsA2Activity.java
+++ app/src/main/java/com/ripka/deutschwiederholung/NounsA2Activity.java
@@ -5,11 +5,13 @@ import java.util.Arrays;
 import java.util.List;
 import com.ripka.deutschwiederholung.models.WordsParser;
 
+import android.graphics.pdf.PdfRenderer;
+
 /**
  * Created by ripka on 9/28/16.
  */
 
-public class NounsB1Activity extends NounsActivity {
+public class NounsA2Activity extends NounsActivity {
     @Override
     protected void afterCreate(boolean isRestored) {
         List<Integer> filesToParse = new ArrayList<>(
