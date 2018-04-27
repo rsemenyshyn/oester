c640cb5749d5c0497cc40f37e1ec02ec29fa2e5a
rsemenyshyn <r.semenyshyn.ua@gmail.com>
2018-04-27 14:33:31 +0200

    added menu for the new words

--- app/src/main/java/com/ripka/deutschwiederholung/NounsB1Activity.java
+++ app/src/main/java/com/ripka/deutschwiederholung/NounsB1Activity.java
@@ -1,19 +1,19 @@
 package com.ripka.deutschwiederholung;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.Arrays;
+import java.util.ArrayList;
 import com.ripka.deutschwiederholung.models.WordsParser;
 
 /**
  * Created by ripka on 9/28/16.
  */
 
-public class NounsA2Activity extends NounsActivity {
+public class NounsB1Activity extends NounsActivity {
     @Override
     protected void afterCreate(boolean isRestored) {
         List<Integer> filesToParse = new ArrayList<>(
-                Arrays.asList(R.raw.nouns_a2)
+                Arrays.asList(R.raw.nouns_b1)
         );
         tests = new WordsParser(filesToParse);
         setNextTest(isRestored);
